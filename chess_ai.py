237ef4e15cfe953ffa589b9b93d959b7761be095
Amr Goher <amrgoher@Amrs-MacBook-Pro.local>
2024-05-27 20:01:22 +0100

    started on eval function

--- chess_ai.py
+++ chess_ai.py
@@ -1,9 +1,33 @@
-import chess_ai
+import chess
 
-# create board object
-board=chess_ai.Board()
 
-# display chess board
+board=chess.Board()
 print(board)
 
 
+# listing all the legal moves in a given state
+legal_moves = list(board.legal_moves)
+for move in legal_moves:
+    print(move)
+
+
+# pick the best move from the list of legal moves
+    
+piece_values = {
+    'p': -1,  # Pawn
+    'n': -3,  # Knight
+    'b': -3,  # Bishop
+    'r': -5,  # Rook
+    'q': -9,  # Queen
+    'k': -100,  # King (considerably large negative value to prioritize king's safety)
+    'P': 1,   # Pawn
+    'N': 3,   # Knight
+    'B': 3,   # Bishop
+    'R': 5,   # Rook
+    'Q': 9,   # Queen
+    'K': 100,  # King (considerably large positive value to prioritize opponent's king's safety)
+    '.': 0    # Empty square
+}
+
+evaluation = sum(piece_values.get(piece, 0) for row in board for piece in row)
+print("Evaluation:", evaluation)
